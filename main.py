bb87efadecf8b1921d0f8fc1787b05b21708abbd
Evgeny Ocean <evgeny.ocean@gmail.com>
2020-10-31 16:51:54 +0300

    first command

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ from discord.ext import commands
 
 # cmon, let's get real here
 intents = discord.Intents.all()
-mia = commands.Bot(command_prefix="$mia", intents=intents)
+mia = commands.Bot(command_prefix="$mia ", intents=intents)
 
 
 @mia.event
-async def on_ready(self):
+async def on_ready():
     print('Mia is ready')
+
+
+@mia.command(name="who")
+async def daddy(ctx, *, command=None):
+    if command == 'is your daddy':
+        await ctx.send('You know that ^^')
+    else:
+        await ctx.send('Figure it out!')
 
 
 mia.run(os.environ.get('MIA_TOKEN'))
