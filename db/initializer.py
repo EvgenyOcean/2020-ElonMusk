cc7cb77d7600f88bfe3326d4f64ac000d894b197
Evgeny Ocean <evgeny.ocean@gmail.com>
2020-11-01 22:50:19 +0300

    deploy attempt #2

--- db/initializer.py
+++ db/initializer.py
@@ -4,5 +4,5 @@ import os
 async def get_pool(elon):
     # credentials = {"user": "postgres", "password": "postgres", "database": "elon", "host": "192.168.99.101"}
     # this will create initial 10 connections to the database
-    pool = await asyncpg.create_pool(dns=os.environ.get('DATABASE_URL'))
+    pool = await asyncpg.create_pool(dsn=os.environ.get('DATABASE_URL'))
     elon.pool = pool
