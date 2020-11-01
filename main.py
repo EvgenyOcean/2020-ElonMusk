9307bcece483b4d66b24350f86e5f54409457e53
Evgeny Ocean <evgeny.ocean@gmail.com>
2020-11-01 20:20:50 +0300

    logging and error handlers

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import discord
 import os
+import logging
 from discord.ext import commands
 from db.initializer import get_pool
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter(r'||||| %(asctime)s: ||||| %(levelname)s:%(message)s')
+handler = logging.FileHandler(filename='./logs/general.logs')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 extensions = (
     'focuser',
@@ -16,16 +23,25 @@ elon = commands.Bot(command_prefix="elon.", intents=intents)
 
 @elon.event
 async def on_ready():
-    print('Elon is ready')
+    logger.info('Elon has been successfully loaded!')
+
+
+@elon.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CommandNotFound):
+        await ctx.send('Command doesn\'t exist!')
+    else:
+        logger.exception('Global Command Catch Exception')
+        await ctx.send('Something went wrong! Try again later!')
 
 
 for extension in extensions:
     try:
         elon.load_extension('cogs.' + extension)
     except Exception as err:
-        print(err)
+        logger.exception(f'{extension} failed to load')
     else:
-        print(extension + ' has been loaded (success)')
+        logger.info(extension + ' has been loaded (success)')
 
 
 # creating a connection pool
